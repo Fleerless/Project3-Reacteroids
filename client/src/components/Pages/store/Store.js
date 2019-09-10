32169c67cf6f2cc103f977940a8e6cbb529609c8
Fleerless <jeffreyfleer@gmail.com>
2019-09-09 20:14:08 -0600

    server calls are working, mongo updates

--- client/src/components/Pages/store/Store.js
+++ client/src/components/Pages/store/Store.js
@@ -8,15 +8,19 @@ export class Store extends Component {
         super(props)
         this.state = {
             username: null,
+            credits: null,
             shipCount: null,
             shipPrice: 500,
+            shipColor: null,
+            shipOutline: null,
+            shipCost: null,
             bulletCount: null,
             bulletPrice: 4000,
             bulletSize: null,
-            credits: null,
-            shipColors: null,
-            shipCost: null
+            bulletCost: null
         }
+        this.updateCosts = this.updateCosts.bind(this)
+
     }
 
     componentDidMount(){
@@ -25,31 +29,77 @@ export class Store extends Component {
             username: this.props.username,
             shipCount: this.props.shipCount,
             bulletCount: this.props.bulletCount,
-            shipCost: this.state.shipCount * this.state.shipPrice,
             bulletSize: this.props.bulletSize
         })
-
-
+        setTimeout(this.updateCosts, 500)
     }
-
+        updateCosts(){
+            let bulletCost = this.state.bulletPrice * this.props.bulletCount;
+            let shipCost = this.state.shipPrice * this.props.shipCount;
+            this.setState({
+                bulletCost: bulletCost,
+                shipCost: shipCost
+            })
+        }
     buyShip(colorsObject){
         let shipCost = this.state.shipPrice * this.state.shipCount;
         let newCredits = this.state.credits - shipCost;
-        this.props.updateUser(colorsObject);
+        let newShipCount = this.state.shipCount++
         this.props.updateUser({
             credits: newCredits,
-            shipCount: this.state.shipCount++
+            shipCount: newShipCount,
+            shipOutline: colorsObject.shipOutline,
+            shipColor: colorsObject.shipColor
         })
         shipCost = this.state.shipPrice * this.state.shipCount;
         this.setState({
             credits: newCredits,
-            shipColors: colorsObject,
+            shipColor: colorsObject.shipColor,
+            shipOutline: colorsObject.shipOutline,
             shipCost: shipCost
         });
         let updateShip = {
-            test: null
+            username: this.state.username,
+            shipOutline: colorsObject.shipOutline,
+            shipColor: colorsObject.shipColor,
+            newCredits: newCredits,
+            newShipCount: newShipCount
+
         }
-        axios.post('/user/storeupdate', updateShip)
+        axios.post('/user/storeupdate/buyship', updateShip)
+        .then(response => {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    };
+
+    buyBullet(){
+        let bulletCost = this.state.bulletPrice * this.state.bulletCount;
+        let newCredits = this.state.credits - bulletCost;
+        let newBulletSize = this.state.bulletSize + 2;
+        let newBulletCount = this.state.bulletCount + 1;
+        this.props.updateUser({
+            credits: newCredits,
+            bulletCount: newBulletCount,
+            bulletSize: newBulletSize
+        })
+        bulletCost = this.state.bulletPrice * this.state.bulletCount;
+        this.setState({
+            credits: newCredits,
+            bulletCost: bulletCost,
+            bulletSize: newBulletSize,
+            bulletCount: newBulletCount
+
+        });
+        let updateBullet = {
+            username: this.state.username,
+            bullet: newBulletSize,
+            newCredits: newCredits,
+            newBulletCount: newBulletCount
+        }
+        axios.post('/user/storeupdate/buybullet', updateBullet)
         .then(response => {
             console.log(response);
         })
@@ -57,33 +107,6 @@ export class Store extends Component {
             console.log(error);
         });
     }
-
-    buyBullet(){
-    let bulletCost = this.state.bulletPrice * this.state.bulletCount;
-    let newCredits = this.state.credits - bulletCost;
-    let newBulletSize = this.state.bulletSize + 2;
-    this.props.updateUser({
-        credits: newCredits,
-        bulletCount: this.state.bulletCount++,
-        bulletSize: newBulletSize
-    })
-    bulletCost = this.state.bulletPrice * this.state.bulletCount;
-    this.setState({
-        credits: newCredits,
-        bulletCost: bulletCost,
-        bulletSize: newBulletSize
-    });
-    let updateBullet = {
-        test: null
-    }
-    axios.post('/user/storeupdate', updateBullet)
-    .then(response => {
-        console.log(response);
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
-}
 
     render(){
         return(
