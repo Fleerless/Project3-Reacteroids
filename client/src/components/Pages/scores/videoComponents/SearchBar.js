0b7f46faf098aa39ba337170ecce94a77ab966a9
Fleerless <jeffreyfleer@gmail.com>
2019-09-11 11:12:50 -0600

    misc updates

--- client/src/components/Pages/scores/videoComponents/SearchBar.js
+++ client/src/components/Pages/scores/videoComponents/SearchBar.js
@@ -21,10 +21,10 @@ class SearchBar extends Component {
                 <label >
                    <p>Use the search field below to find videos on various Asteroids versions over the years:</p>
                    <ul>
+                       <li>Asteroids 1979 </li>
                        <li>Asteroids Deluxe 1981 </li>
                        <li>Space Duel 1982 </li>
                        <li>Blasteroids 1987 </li>
-                       <li>Asteroids 1979 </li>
 
                            </ul> 
                     <input className = "searchHolder"
