0d9478f3247b4f76fe4b5171ed6a2028447632ca
Jeffrey Fleer <jeffreyfleer+github@gmail.com>
2020-01-17 18:50:15 -0700

    changed comment to exclude if statement

--- client/src/components/Pages/scores/videoComponents/VideoDetail.js
+++ client/src/components/Pages/scores/videoComponents/VideoDetail.js
@@ -1,18 +1,18 @@
 import React from "react";
 
 const VideoDetail = ({ selectedVideo }) => {
-    // if (!selectedVideo) return <div>
-    //     <h3>go to client/src/components/Pages/scores/utils/API.js...
-    //         to paste the valid API key that is
-    //         commented out in that file</h3>
-    //     <h2>NOTE!!!: Remember to pull the API key out of "const apiKey" after confirming the
-    //         VideoGrid component works... otherwise, EVERY SINGLE CODE KEYSTROKE
-    //         afterwards will generate an API call, and the daily quota will be
-    //         EXCEEDED with about 40 keystrokes!!!
-    //         (and, the VideoGrid will show what you are now reading... and NOT FUNCTION...)
+    if (!selectedVideo) return <div>
+        {/* <h3>go to client/src/components/Pages/scores/utils/API.js...
+            to paste the valid API key that is
+            commented out in that file</h3>
+        <h2>NOTE!!!: Remember to pull the API key out of "const apiKey" after confirming the
+            VideoGrid component works... otherwise, EVERY SINGLE CODE KEYSTROKE
+            afterwards will generate an API call, and the daily quota will be
+            EXCEEDED with about 40 keystrokes!!!
+            (and, the VideoGrid will show what you are now reading... and NOT FUNCTION...)
 
-    // </h2>
-    // </div>
+    </h2> */}
+    </div>
 
 
 
